569f74a4ec7d536a603723d2ad49386593295e68
Miguel Huerta <miguelhuerta@Miguels-MBP.attlocal.net>
2019-11-07 17:38:57 -0800

    books migration in place. Had to do some config changes for AppServiceProvider and also mysql connection so it could work with MAMP

--- app_2/config/database.php
+++ app_2/config/database.php
@@ -54,6 +54,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock'
         ],
 
         'pgsql' => [
