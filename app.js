a9631f83b32a09c4eddb16a3cac73e1eba340a67
Miguel Huerta <miguelhuerta@Miguels-MBP.attlocal.net>
2019-11-07 16:12:33 -0800

    app.js in place. Rendering out list of books.

--- app.js
+++ app.js
@@ -3,6 +3,23 @@
 var app = new Vue({
 	el : "#app",
 	data : {
-		message : 'Miguel you did it!'
+		heading: "Welcome to the Book Store",
+		books: [
+			{
+				title: "Harry Potter Chamber of Secrets",
+				author: "J.K Rowling",
+				published: "2008"
+			},
+			{
+				title: "Bourne Supremacy",
+				author: "Miguel Huerta",
+				published: "2014"
+			},
+			{
+				title: "James Bond",
+				author: "Susan Espino",
+				published: "2018"
+			}
+		]
 	}
 });
