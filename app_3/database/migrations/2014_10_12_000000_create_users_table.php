faa6ab1342a195322569494587099a3683f6d526
Miguel Huerta <miguelhuerta@Miguels-MBP.attlocal.net>
2019-11-10 19:40:03 -0800

    saving point. update users migrations and installed passport

--- app_3/database/migrations/2014_10_12_000000_create_users_table.php
+++ app_3/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('username', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address', 256);
+            $table->string('city', 100);
+            $table->char('state', 2);
+            $table->string('zipcode', 7);
             $table->rememberToken();
             $table->timestamps();
         });
